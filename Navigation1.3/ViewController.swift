f20abe9da3b6d97f9019173cc29724f85efecfc1
harryhole13 <harryhole13@gmail.com>
2022-06-05 15:18:10 +0300

    Initial Commit

---
+++ Navigation1.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation1.3
+//
+//  Created by Алексей Потемин on 05.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
